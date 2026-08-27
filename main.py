fb2dfdb5548f58195baefe7ae390d1d88833c09e
Jodie Burchell <jodie.burchell@gmail.com>
2026-08-27 12:31:41 +0200

    Add support for monitoring changes in "pdf_language_learner" and "static" directories during development

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("pdf_language_learner.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "pdf_language_learner.app:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
+        reload_dirs=["pdf_language_learner", "static"],
+    )
